dbd621ddef06b1c74d3fb3547d57e7ff63d1402f
abhiteja <abhitejag@gmail.com>
2017-11-11 09:41:36 -0800

    worked on state and props

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
 
-import App from './components/app';
 import reducers from './reducers';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 const API_KEY = 'AIzaSyAt52r0x7CAWFfxJppkhcix9hVwtRSd5og';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <SearchBar />
-  </Provider>
-  , document.querySelector('.container'));
+class Ytube extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      videos: [], 
+      selectedVideo: null
+    };
+    this.videoSearch('cricket');
+  }
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+  render(){
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) } , 300);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail  video={this.state.selectedVideo}/>
+        <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo}) } videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Ytube />, document.querySelector('.container'));

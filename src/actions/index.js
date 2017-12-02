0c712abb464408b541226a310d9ecff5958dabcf
abhiteja <abhitejag@gmail.com>
2017-12-02 13:39:14 -0800

    weather report graph with weather api, google map and charts

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const API_KEY = 'aa0133472fc371ee256199cc1df539d7';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
